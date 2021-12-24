f497a9afccde99f2006ca83652addb9bc5c444fa
Blaekmare <94797990+Blaekmare@users.noreply.github.com>
2021-12-24 16:32:47 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Elpodpis.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Elpodpis.dir/main.cpp.obj"
+  "Elpodpis.exe"
+  "Elpodpis.exe.manifest"
+  "Elpodpis.pdb"
+  "libElpodpis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Elpodpis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/md5_clion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/md5_clion.dir/MD5.cpp.obj"
+  "CMakeFiles/md5_clion.dir/main.cpp.obj"
+  "libmd5_clion.dll.a"
+  "md5_clion.exe"
+  "md5_clion.exe.manifest"
+  "md5_clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/md5_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
